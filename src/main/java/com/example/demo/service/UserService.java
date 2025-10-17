4e8b0691dc2fad4c6e13a3c9a81160bcf7c3c87e
topslugBurpy <topslugger6969@gmail.com>
2025-10-18 01:36:11 +0530

    feat: exception and response

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    List<UserDto> getAllUsers();
     String addUser(UserCreateDto userCreateDto);
     boolean verifyUser(UserLoginDto userLoginDto);
+    boolean ifUserExists(UserCreateDto userCreateDto);
 }
